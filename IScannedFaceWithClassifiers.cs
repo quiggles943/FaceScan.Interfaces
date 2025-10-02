91e0fad0d7e3c76d7c90a73df07ad01f18c9a791
Paul Quigley <paul.buchanan-quigley@outlook.com>
2025-10-02 20:38:28 +0100

    Added new scannedFace interface which includes age and gender classifiers

---
+++ IScannedFaceWithClassifiers.cs
@@ -0,0 +1,15 @@
+﻿using FaceScan.Interfaces.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaceScan.Interfaces
+{
+    public interface IScannedFaceWithClassifiers: IScannedFace
+    {
+        public Gender? GetGender();
+        public int? GetAge();
+    }
+}
